8ee81649e089d3b36069884795ff583e7499607a
auron567 <xauron567x@gmail.com>
2019-06-12 10:46:59 +0200

    Replace Handler with LiveData's postValue method

--- app/src/main/java/com/example/kotlinbackgroundprocessing/repository/BasicRepository.kt
+++ app/src/main/java/com/example/kotlinbackgroundprocessing/repository/BasicRepository.kt
@@ -1,9 +1,5 @@
 package com.example.kotlinbackgroundprocessing.repository
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.kotlinbackgroundprocessing.app.PhotosUtils
@@ -22,24 +18,12 @@ object BasicRepository : Repository {
     }
 
     private fun fetchJsonData() {
-        val handler = object : Handler(Looper.getMainLooper()) {
-            override fun handleMessage(msg: Message?) {
-                val bundle = msg?.data
-                val photos = bundle?.getStringArrayList("PHOTO_KEY")
-                photosLiveData.value = photos
-            }
-        }
-
         val runnable = Runnable {
             val photosString = PhotosUtils.photoJsonString()
             val photos = PhotosUtils.photoUrlsFromJsonString(photosString)
 
             if (photos != null) {
-                val message = Message()
-                val bundle = Bundle()
-                bundle.putStringArrayList("PHOTO_KEY", photos)
-                message.data = bundle
-                handler.sendMessage(message)
+                photosLiveData.postValue(photos)
             }
         }
 
