ddbd40940d8e1c4904460c0dbfaa1883be924f84
auron567 <xauron567x@gmail.com>
2019-07-11 19:21:21 +0200

    Add notification to SongService

--- app/src/main/java/com/example/kotlinbackgroundprocessing/service/SongService.kt
+++ app/src/main/java/com/example/kotlinbackgroundprocessing/service/SongService.kt
@@ -1,18 +1,37 @@
 package com.example.kotlinbackgroundprocessing.service
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.kotlinbackgroundprocessing.R
 import com.example.kotlinbackgroundprocessing.app.App
 import com.example.kotlinbackgroundprocessing.app.SongUtils
+import com.example.kotlinbackgroundprocessing.ui.main.MainActivity
 
 class SongService : Service() {
+
+    companion object {
+        private const val CHANNEL_NAME = "Media playback"
+        private const val CHANNEL_ID = "MEDIA_PLAYBACK"
+        private const val NOTIFICATION_TITLE = "Playing song"
+    }
+
     private lateinit var mediaPlayer: MediaPlayer
 
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startForeground(1000, createNotification())
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -28,5 +47,37 @@ class SongService : Service() {
         mediaPlayer.stop()
         App.isPlayingSong = false
         super.onDestroy()
+    }
+
+    private fun createNotification(): Notification {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel()
+        }
+
+        val notificationIntent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+        val intent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentIntent(intent)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setOnlyAlertOnce(true)
+            .setContentTitle(NOTIFICATION_TITLE)
+            .build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        val name = CHANNEL_NAME
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            setShowBadge(false)
+            lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        }
+
+        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+        notificationManager?.createNotificationChannel(channel)
     }
 }
